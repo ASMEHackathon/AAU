655d135fdf966b3dbcd589c6146e9e7f92305777
mail4umar <46414488+mail4umar@users.noreply.github.com>
2020-08-16 14:50:10 -0500

    Update extractingdata.m

--- pre-processed/extractingdata.m
+++ pre-processed/extractingdata.m
@@ -1,7 +1,7 @@
-A = xlsread('bridgeport1week1-train.csv');
+A = xlsread('bridgeport2week1-train.csv');
 BP1w1=A;
 clear A
-A = xlsread('bridgeport1week2-train.csv');
+A = xlsread('bridgeport2week2-train.csv');
 BP1w2=A;
 clear A
 All=[BP1w1;BP1w2];
@@ -13,7 +13,7 @@ yD=All(:,5);
 t1=All(:,1:2);
 t2=All(:,3);
 b=All(:,16);
-A = xlsread('bridgeport1week3-test.csv');
+A = xlsread('bridgeport2week3-test.csv');
 test_X=[A(:,7:8) A(:,11:12) A(:,6)];
 test_Y=[A(:,13) A(:,4:5) A(:,9:10)];
 T1=[t1;A(:,1:2)];
@@ -28,4 +28,7 @@ clear yI
 yI=y;
 Y=[YI(:,4) YI(:,5) YI(:,2) YI(:,3) YI(:,1)];
 YI=Y;
+clear A
+A=test_Y;
+test_Y=[A(:,4) A(:,5) A(:,2) A(:,3) A(:,1)];
 clear A
